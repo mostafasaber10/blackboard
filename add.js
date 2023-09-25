7a707a3847163ce67f6c0fb0a36d081790bfc566
mostafasaber10 <109151913+mostafasaber10@users.noreply.github.com>
2023-09-25 18:57:44 +0300

    Add files via upload

--- add.js
+++ add.js
@@ -32,152 +32,149 @@ const productButton = document.querySelectorAll('.purchase-btn')
 
 
 
-  // متغير يخزن عدد العناصر في السلة
-  let cartdetails = 0;
+ // متغير يخزن عدد العناصر في السلة
+let cartdetails = 0;
 
- // مصفوفة لتخزين التفاصيل الخاصة بالعناصر في السلة
-  const cartItems = [];
+// مصفوفة لتخزين التفاصيل الخاصة بالعناصر في السلة
+const cartItems = [];
 
+// احصل على عناصر الواجهة
+const sgi = document.querySelector('.sgi');
+const quantityElement = document.querySelector('.quantity');
+const popup = document.querySelector('.popup');
+const close = document.querySelector('.close');
+const cartDetailsList = document.querySelector('.cart-details');
+const totalDetailsPrice = document.querySelector('.total-price-details');
 
-
-let pressure = document.querySelector('.sgi');
-let zero = document.querySelector('.quantity');
-let close = document.querySelector('.close');
-let list = document.querySelector('.cart-details');
-let total = document.querySelector('.total-price-details');
-let purch = document.querySelector('.purchase-btn');
-let popup = document.querySelector('.popup');
-
-
-
-pressure.addEventListener("click" ,()=>
-{
-    openCartWindow();
-}
-
- )
-
- function openCartWindow() {
-         let popup = document.querySelector('.popup');
-         let close = document.querySelector('.close');
-         let list = document.querySelector('.cart-details');
-        let totalDetailsPrice = document.querySelector('.total-price-details');
-        list.innerHTML = '';
-            let total = 0;
-            
-            cartItems.forEach((item, index) => {
-                const listItem = document.createElement('li');
-                listItem.innerHTML = `
-                    <span class="delete-item" onclick="removeItem(${index})">(X)</span>
-                    <img src="${item.image}" alt="${item.name}" />
-                    ${item.name} - $${item.price.toFixed(2)}
-                `;
-                list.appendChild(listItem);
-                total += item.price;
-            });
+// احصل على جميع أزرار زيادة ونقص الكمية
+const increaseQuantityButtons = document.querySelectorAll('.increase-quantity');
+const decreaseQuantityButtons = document.querySelectorAll('.decrease-quantity');
+const deleteItemButtons = document.querySelectorAll('.delete-item'); // أزرار حذف المنتج
 
 
-            
+window.addEventListener('load', () => {
+    const cartVisible = localStorage.getItem('cartVisible');
+    if (cartVisible === 'visible') {
+        popup.style.display = 'block';
+    } else {
+        popup.style.display = 'none';
+    }
+});
 
-            
+
+
+// عند النقر على الزرار لإظهار القائمة
+sgi.addEventListener('click', () => {
+    // إذا كانت القائمة مرئية، قم بإخفائها وقم بتحديث localStorage
+    if (popup.style.display === 'block') {
+        popup.style.display = 'none';
+        localStorage.setItem('cartVisible', 'hidden');
+    } else {
+        // إذا كانت القائمة مختفية، قم بإظهارها وقم بتحديث localStorage
+        popup.style.display = 'block';
+        localStorage.setItem('cartVisible', 'visible');
+        // هنا يمكنك إضافة الكود الخاص بعرض التفاصيل في القائمة
+    }
+});
+
+
+
+
+
+
+
+
+// الدالة التي تفتح نافذة السلة
+function openCartWindow() {
+    // مسح قائمة السلة
+    cartDetailsList.innerHTML = '';
+    
+    let total = 0;
+    
+    cartItems.forEach((item, index) => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
         
-  // قم بتحديث المجموع في تفاصيل السلة
-  totalDetailsPrice.textContent = `المجموع: $${total.toFixed(2)}`;
+        <span>اسم المنتج: ${item.name}</span><br>
+            <span>النوع: ${item.type}</span><br>
+            <span>السعر: $${item.price.toFixed(2)}</span><br>
+            <span>الكمية: <span class="quantity">${item.quantity}</span></span>
+            <button class="increase-quantity" onclick="increaseQuantity(${index})">+</button>
+            <button class="decrease-quantity" onclick="decreaseQuantity(${index})">-</button>
+            <button class="delete-item" onclick="deleteItem(${index})">(X)</button> <!-- زر حذف المنتج -->
+            `;
+        cartDetailsList.appendChild(listItem);
+        total += item.price * item.quantity;
+    });
 
-     // إظهار النافذة المنبثقة
+    // تحديث المجموع في تفاصيل السلة
+    totalDetailsPrice.textContent = `المجموع: $${total.toFixed(2)}`;
+
+    // إظهار النافذة المنبثقة
     popup.style.display = 'block';
 
-     
     close.addEventListener('click', () => {
         popup.style.display = 'none';
     });
-   
- 
- 
- }
-
-
- function addToCart(item) {
-    cartItems.push(item);
-    
-    // زيادة قيمة العنصر quantity بمقدار 1
-    cartdetails++;
-    
-    // تحديث نص عدد المنتجات في السلة
-    const quantityElement = document.querySelector('.quantity');
-    quantityElement.textContent = cartdetails;
 }
 
-let quantity = 0; // المتغير الذي سيخزن العدد
-
-function increaseQuantity() {
-    quantity++;
-    updateQuantity();
+// دالة لزيادة كمية المنتج
+function increaseQuantity(index) {
+    cartItems[index].quantity++;
+    updateCart();
 }
 
-function decreaseQuantity() {
-    if (quantity > 0) {
-        quantity--;
-        updateQuantity();
+// دالة لنقص كمية المنتج
+function decreaseQuantity(index) {
+    if (cartItems[index].quantity > 0) {
+        cartItems[index].quantity--;
+        updateCart();
     }
 }
 
-function updateQuantity() {
-    const quantityElement = document.querySelector('.quantity');
-    quantityElement.textContent = quantity;
+// دالة لحذف المنتج
+function deleteItem(index) {
+    cartItems.splice(index, 1);
+    cartdetails--;
+    updateCart();
 }
 
 
-const newItem = {
-    name: 'اسم المنتج',
-    price: 10.00,
-    image: 'رابط الصورة' 
-    
-};
-addToCart(newItem);
+// دالة لإضافة منتج إلى السلة
+function addToCart(item) {
+    cartItems.push(item);
+    cartdetails++;
+    updateCart();
+}
 
-const newIte = {
-    name: 'اسم المنتج',
-    price: 20.00,
-    image: 'رابط الصورة'
-};
-addToCart(newIte);
-
-
-const newIt = {
-    name: 'اسم المنتج',
-    price: 30.00,
-    image: 'رابط الصورة'
-};
-addToCart(newIt);
-
-
-const newI= {
-    name: 'اسم المنتج',
-    price: 40.00,
-    image: 'رابط الصورة'
-};
-addToCart(newI);
-
-
-function removeItem(index) {
-    cartItems.splice(index, 1);
-    cartdetails--;
+// دالة لتحديث نص عدد المنتجات في السلة
+function updateCart() {
     const quantityElement = document.querySelector('.quantity');
     quantityElement.textContent = cartdetails;
     openCartWindow();
 }
 
- // دالة لإغلاق النافذة المنبثقة
- function closeCartWindow() {
-    let popup = document.querySelector('.popup');
-    popup.style.display = 'none';
-  }
 
 
+// إضافة عناصر إلى السلة
+const newItem1 = {
+    name: 'منتج 1',
+    type: 'نوع المنتج 1',
+    price: 20.00,
+    quantity: 1,
+};
+addToCart(newItem1);
 
+const newItem2 = {
+    name: 'منتج 2',
+    type: 'نوع المنتج 2',
+    price: 25.00,
+    quantity: 1,
+};
+addToCart(newItem2);
 
-
+// الزر الذي يفتح نافذة السلة عند النقر عليه
+sgi.addEventListener('click', openCartWindow);
 
 
 
